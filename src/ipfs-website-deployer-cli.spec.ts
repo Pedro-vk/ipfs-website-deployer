edf9b92678f8d210f6f921ae779c610f1f7d0b5e
Pedro Gutiérrez <pedrogp9693@gmail.com>
2019-03-22 01:29:52 +0100

    Add tests on cli.

---
+++ src/ipfs-website-deployer-cli.spec.ts
@@ -0,0 +1,14 @@
+import 'mocha';
+import { assert } from 'chai';
+import { promisify } from 'util';
+import { exec } from 'child_process';
+
+const run = promisify(exec);
+
+describe('ipfs-website-deployer-cli', () => {
+  it('should deploy a folder and return a hash', async () => {
+    const {stdout} = await run('npx ts-node src/ipfs-website-deployer-cli.ts --only-hash src')
+
+    assert.match(stdout, /^[a-z0-9]{46}\n$/i);
+  });
+});
