66cda0bfa75c377d9afa982d953dc18a1e9caead
Pedro Gutiérrez <pedrogp9693@gmail.com>
2019-03-22 03:27:23 +0100

    Return a json with the deployment data.

--- src/ipfs-website-deployer-cli.spec.ts
+++ src/ipfs-website-deployer-cli.spec.ts
@@ -7,14 +7,23 @@ const run = promisify(exec);
 
 describe('ipfs-website-deployer-cli', () => {
   it('should deploy a folder and return a hash', async () => {
-    const {stdout} = await run('npx ts-node src/ipfs-website-deployer-cli.ts --only-hash src')
+    const {stdout} = await run('npx ts-node src/ipfs-website-deployer-cli.ts --only-hash src');
 
-    assert.match(stdout, /^[a-z0-9]{46}\n$/i);
+    assert.match(stdout.trim(), /^[a-z0-9]{46}$/i);
   });
 
   it('should deploy a folder with final slash', async () => {
-    const {stderr} = await run('npx ts-node src/ipfs-website-deployer-cli.ts --only-hash src/')
+    const {stderr} = await run('npx ts-node src/ipfs-website-deployer-cli.ts --only-hash src/');
 
     assert.isEmpty(stderr.trim());
+  });
+
+  it('should print a json', async () => {
+    const {stdout} = await run('npx ts-node src/ipfs-website-deployer-cli.ts --json src/');
+    const result = JSON.parse(stdout);
+
+    assert.isDefined(result);
+    assert.match(result.rootHash, /^[a-z0-9]{46}$/i);
+    assert.instanceOf(result.files, Array);
   });
 });
--- src/ipfs-website-deployer-cli.ts
+++ src/ipfs-website-deployer-cli.ts
@@ -14,13 +14,14 @@ program
   .option('-p, --port <port>', 'IPFS port', 5001)
   .option('--protocol <http|https|...>', 'IPFS protocol', 'https')
   .option('--only-hash', 'Returns only root hash.')
+  .option('--json', 'Returns full JSON data.')
   .action(async (folder) => {
     if (typeof folder !== 'string') {
       console.error('Path not defined. Use `--help`,');
       process.exit(1);
     }
-    const {host, port, protocol, onlyHash} = program;
-    await deploy(folder, host, +port, protocol, onlyHash);
+    const {host, port, protocol, onlyHash, json} = program;
+    await deploy(folder, host, +port, protocol, onlyHash, json);
   })
   .parse(process.argv);
 
@@ -30,21 +31,28 @@ async function deploy(
   port?: number,
   protocol?: string,
   onlyHash?: boolean,
+  json?: boolean,
 ) {
   const config = {host, port, protocol};
   Object.keys(config)
       .forEach((key) => config[key] || delete config[key]);
 
-  if (!onlyHash) {
-    console.log(`Deploying files inside "${folder}"`);
+  if (!onlyHash && !json) {
+    console.log(`Deploying files inside "${folder}".`);
   }
   const ipfsDeployer = new IpfsDeployer(config);
   const result = await ipfsDeployer.deployFolder(folder);
 
-  if (onlyHash) {
-    console.log(result.rootHash);
-  } else {
-    console.log(`Deployed ${result.files.length} nodes.`);
-    console.log(`Root hash: ${result.rootHash}`);
+  switch (true) {
+    case json:
+      console.log(JSON.stringify(result, null, 2));
+      break;
+    case onlyHash:
+      console.log(result.rootHash);
+      break;
+    default:
+      console.log(`Deployed ${result.files.length} nodes.`);
+      console.log(`Root hash: ${result.rootHash}`);
+      break;
   }
 }
