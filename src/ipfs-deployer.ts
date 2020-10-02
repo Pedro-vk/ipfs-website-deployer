507f63fa57d3b5ce80b860fa9a9c334ca66718d8
Pedro Gutiérrez <pedrogp9693@gmail.com>
2020-10-02 21:19:13 +0200

    Fix linter issues

--- src/ipfs-deployer.ts
+++ src/ipfs-deployer.ts
@@ -1,5 +1,5 @@
-import ipfsHttpClient from 'ipfs-http-client';
 import glob from 'glob';
+import ipfsHttpClient from 'ipfs-http-client';
 
 export interface IpfsDeployerConfig {
   host: string;
@@ -41,19 +41,22 @@ export class IpfsDeployer {
     this.ipfs = new ipfsHttpClient({...defaultConfig, ...config});
   }
 
-  async deployFiles(files: IpfsDeployerFile[], onProgress?: (progress: IpfsDeployerProgress) => void): Promise<IpfsDeployerResult> {
-    const progress = await this.getProgressHandler(undefined, files.length, onProgress)
-    let results = await this.ipfs
+  async deployFiles(
+    files: IpfsDeployerFile[],
+    onProgress?: (progress: IpfsDeployerProgress) => void,
+  ): Promise<IpfsDeployerResult> {
+    const progress = await this.getProgressHandler(undefined, files.length, onProgress);
+    const results = await this.ipfs
       .add(
         files.map((file) => ({...file, path: `root/${file.path}`})),
-        {progress}
+        {progress},
       );
     return this.getResultFromListOfFiles(results);
   }
 
   async deployFolder(path: string, onProgress?: (progress: IpfsDeployerProgress) => void): Promise<IpfsDeployerResult> {
-    const progress = await this.getProgressHandler(path, undefined, onProgress)
-    const results = await this.ipfs.addFromFs(path, {recursive: true, progress})
+    const progress = await this.getProgressHandler(path, undefined, onProgress);
+    const results = await this.ipfs.addFromFs(path, {recursive: true, progress});
     return this.getResultFromListOfFiles(results);
   }
 
@@ -72,23 +75,23 @@ export class IpfsDeployer {
     return () => {
       progress++;
       onProgress({total, progress});
-    }
+    };
   }
 
   private getFilesInFolderNumber(path: string) {
-    return new Promise<number>(resolve => {
+    return new Promise<number>((resolve) => {
       glob(`${path.replace(/\/$/, '')}/**/*`, {nodir: true}, (err, result) => resolve(result.length));
-    })
+    });
   }
 
   private getResultFromListOfFiles(nodes: IpfsDeployerNode[]): IpfsDeployerResult {
-    const rootFolder = (nodes[0].path.match(/^([^\/]+)/) as any)[0]
+    const rootFolder = (nodes[0].path.match(/^([^\/]+)/) as any)[0];
     nodes = nodes
       .map((file) => ({
         ...file,
         path: file.path.replace(new RegExp(`^${rootFolder}\/?`), './'),
       }));
-    const root = nodes.find(({path}) => path === './')
+    const root = nodes.find(({path}) => path === './');
     const rootHash = (root || {} as any).hash;
     return {rootHash, nodes};
   }
